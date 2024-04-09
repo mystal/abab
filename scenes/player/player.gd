1d9271135fe39e34c82472f0589ccc8a3c87eb19
Gabriel Martinez <reitaka@gmail.com>
2024-04-09 09:17:15 -0700

    Add a very quick, janky slide ability

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -36,9 +36,13 @@ enum Facing {
 ## Jump height in tiles.
 @export var JUMP_HEIGHT_TILES: float = 3.5
 ## Jump horizontal distance in tiles.
-@export var JUMP_DISTANCE_TILES: float = 5
+@export var JUMP_DISTANCE_TILES: float = 5.0
 ## What to do when releasing the jump button after jumping.
 @export var JUMP_RELEASE_BEHAVIOR: JumpReleaseBehavior = JumpReleaseBehavior.None
+
+@export_group("Slide")
+@export var SLIDE_DISTANCE_TILES: float = 3.5
+@export var SLIDE_DURATION: float = 0.3
 
 # Derived jump constants.
 var JUMP_HEIGHT: float = JUMP_HEIGHT_TILES * TILE_HEIGHT
@@ -51,6 +55,9 @@ var HALF_TILE_JUMP_PEAK_TIME: float = sqrt(TILE_HEIGHT / GRAVITY)
 var HALF_TILE_JUMP_SPEED: float = -(GRAVITY * HALF_TILE_JUMP_PEAK_TIME)
 var JUMP_RELEASE_SPEED: float = 0.0
 
+# Derived slide constants.
+var SLIDE_SPEED: float = (SLIDE_DISTANCE_TILES * TILE_WIDTH) / SLIDE_DURATION
+
 @onready var _lives: int = INITIAL_LIVES
 @onready var _facing: Facing = INITIAL_FACING
 @onready var _respawn_position: Vector2 = position
@@ -58,6 +65,7 @@ var JUMP_RELEASE_SPEED: float = 0.0
 
 var _num_pew_pews: int = 0
 var _dead: bool = false
+var _slide_time: float = 0.0
 
 # Input action vars
 var _jump_input: String = "jump"
@@ -86,8 +94,10 @@ func _physics_process(delta):
 	if _dead:
 		return
 
-	# In-air logic.
+	var move_direction = Input.get_axis(_move_left_input, _move_right_input)
+
 	if not is_on_floor():
+		# In-air logic.
 		# TODO: Add a minimum jump release time.
 		if _can_release_jump() and Input.is_action_just_released(_jump_input) and velocity.y < JUMP_RELEASE_SPEED:
 			# Clamp velocity if greater than JUMP_RELEASE_SPEED.
@@ -95,24 +105,40 @@ func _physics_process(delta):
 		else:
 			# Apply gravity.
 			velocity.y += GRAVITY * delta
-
-	# Handle Jump and Slide.
-	if Input.is_action_just_pressed(_jump_input) and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# TODO: Add acceleration/deceleration.
-	var direction = Input.get_axis(_move_left_input, _move_right_input)
-	if direction:
-		velocity.x = direction * MOVE_SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, MOVE_SPEED)
+		# Grounded logic.
+		if Input.is_action_just_pressed(_jump_input):
+			# Start Jump
+			velocity.y = JUMP_VELOCITY
+		elif Input.is_action_just_pressed(_slide_input):
+			# Start Slide
+			_slide_time = SLIDE_DURATION
+			var direction = sign(move_direction)
+			if direction == 0.0:
+				if _facing == Facing.Right:
+					direction = 1.0
+				else:
+					direction = -1.0
+			velocity.x = direction * SLIDE_SPEED
+
+	if is_sliding():
+		# Handle slide logic.
+		_slide_time -= delta
+	else:
+		# Handle standard movement/deceleration.
+		# TODO: Add acceleration/deceleration.
+		if move_direction:
+			velocity.x = move_direction * MOVE_SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, MOVE_SPEED)
 
 	# Modifies velocity
 	move_and_slide()
 
 	# Update animation
-	if !is_on_floor():
+	if is_sliding():
+		_sprite.play("slide")
+	elif !is_on_floor():
 		_sprite.play("jump")
 	elif is_zero_approx(velocity.x):
 		_sprite.play("idle")
@@ -139,6 +165,9 @@ func _physics_process(delta):
 			pew_pew.SPEED *= -1
 		get_tree().current_scene.add_child(pew_pew)
 		_num_pew_pews += 1
+
+func is_sliding() -> bool:
+	return _slide_time > 0.0
 
 func _can_release_jump() -> bool:
 	return JUMP_RELEASE_BEHAVIOR != JumpReleaseBehavior.None
@@ -169,6 +198,7 @@ func _respawn():
 	$AnimatedSprite2D.visible = true
 	$WorldCollision2D.set_deferred("disabled", false)
 	$Area2D/AreaCollision2D.set_deferred("disabled", false)
+	_slide_time = 0.0
 	_dead = false
 
 func _on_area_2d_body_entered(body: Node2D):
